e8a2bdb88b2d534e16276792a053a584a6a29810
zl zhang <zhangzhuoli2001@outlook.com>
2023-04-10 11:14:59 +0800

    k均值

--- kmeans/kmeans.py
+++ kmeans/kmeans.py
@@ -1,27 +1,33 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+# 设置图像中文字体为黑体
+mpl.rc("font", family="SimHei")
+# 解决坐标轴负号不显示
+mpl.rcParams['axes.unicode_minus'] = False
 
 
 def dist(a, b):
-    """ a和b点的距离 """
-    return numpy.dot(a - b, a - b)
+    """ a和b点的距离的平方 """
+    return np.dot(a - b, a - b)
 
 
 # 聚类k个数和数据n个数
 k = 5
 n = 100
 # 生成的数据矩阵,最后一列是点的标签
-data = numpy.random.randn(n, 3) * 10
+data = np.random.randn(n, 3) * 10
 for i in range(0, n):
     data[i, 2] = -1
 # 选取初始向量
-mu = numpy.random.randint(low=0, high=n, size=k)
-mean = numpy.full((k, 2), 0.0)
+mu = np.random.randint(low=0, high=n, size=k)
+mean = np.full((k, 2), 0.0)
 for i in range(0, k):
     for j in (0, 1):
         mean[i, j] = data[mu[i], j]
 # 点到均值向量的距离矩阵
-distm = numpy.full((n, k), 0.0)
+distm = np.full((n, k), 0.0)
 # 迭代
 iter = 500
 for t in range(0, iter):
@@ -31,7 +37,7 @@ for t in range(0, iter):
             distm[i, j] = dist(data[i, 0:2], mean[j, :])
     # 分类
     for i in range(0, n):
-        data[i, 2] = numpy.argmin(distm[i, :])
+        data[i, 2] = np.argmin(distm[i, :])
     # 更新均值向量
     for i in range(0, k):
         smean = (0.0, 0.0)
@@ -42,6 +48,13 @@ for t in range(0, iter):
                 num = num + 1
         if num != 0:
             mean[i, :] = [i / num for i in smean]
+# 均方损失函数
+loss = 0
+for i in range(0, k):
+    for j in range(0, n):
+        if data[j, 2] == i:
+            loss = loss + dist(data[j, 0:2], mean[i, :])
+print("原损失函数值: ", loss)
 # 数据可视化
 colors = ['red', 'green', 'blue', 'purple', 'yellow']
 for i in range(0, k):
@@ -53,4 +66,5 @@ for i in range(0, k):
             y.append(data[j, 1])
     plt.scatter(x, y, c=colors[i], s=10)
     plt.scatter(mean[i, 0], mean[i, 1], c=colors[i], s=100)
+plt.title("原k均值分类结果")
 plt.show()
